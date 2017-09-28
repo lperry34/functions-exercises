5d1d3d9d1e7cb3f60d11a2e0ff9412beece85232
lperry34 <leviperry34@gmail.com>
2017-09-28 13:16:55 -0700

    more

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 // TODO: add your solutions here!
+//concat
 function cat(string, andString){
     return (string.concat(andString));
 }
@@ -7,7 +8,7 @@ var string = "hello";
 var andString = "there";
 cat(string, andString);
 
-//next function
+//repeat
 
 function repeat(string, n) {
 	for(var i = 0; i < n; i++) {
@@ -19,7 +20,7 @@ var string = "hello";
 var n = 5;
 repeat(string, n);
 
-//next function
+//power of
 
 function power(number, pow){
     return number ** pow;
@@ -29,7 +30,7 @@ var number = 2;
 var pow = 4;
 power(number, pow);
 
-//next function
+//Circle Area
 
 function area(radius){
     return (radius ** 2) * (3.14);
@@ -38,4 +39,35 @@ function area(radius){
 var radius = 2;
 area(radius);
 
-// next function
+
+
+// pythagorean theorem
+
+function coolMath(a, b){
+    return (a ** 2) + (b ** 2);
+}
+
+var a = 2;
+var b = 3;
+var c = a + b;
+coolMath(a, b);
+
+
+// is x divisable by y
+
+function isDivisible(x, y){
+    if(x%y == 0){
+        return true;
+    } else{
+        return false;
+    }
+
+}
+
+var x = 9;
+var y = 3;
+isDivisible(x, y);
+
+
+
+
